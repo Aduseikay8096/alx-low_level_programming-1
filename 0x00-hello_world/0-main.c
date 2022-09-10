adc795b8a156e43e4dab5e857191cf53be80ca0b
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-09 21:13:34 -0400

    Meet the Betty code standard

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello, ALX World!");
-	return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Coding is fun, but challenging!");
+	return (0);
 }
