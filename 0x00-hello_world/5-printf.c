eb64054c755eec4d13c9a131de10ea0767d96ced
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-11 18:03:41 -0400

    Print size of each data type

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -19,5 +19,15 @@ int main(void)
 	long int i;
 	long long int j;
 
-
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a short: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a unsigned short: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(g));
+	printf("Size of a unsigned long: %lu byte(s)\n", (unsigned long)sizeof(h));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(j));
+	return (0);
 }
