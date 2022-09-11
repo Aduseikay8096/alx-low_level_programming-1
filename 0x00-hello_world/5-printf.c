d7644acbecbe65ff162e5ae5383705822e85bfda
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-11 18:23:15 -0400

    Restore 5-printf.c algorithm

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,27 +6,6 @@
  */
 int main(void)
 {
-	// Data types
-	char a;
-	double b;
-	float c;
-	int d;
-	short e;
-	unsigned short f;
-	long g;
-	unsigned long h;
-	long int i;
-	long long int j;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a short: %lu byte(s)\n", (unsigned long)sizeof(e));
-	printf("Size of a unsigned short: %lu byte(s)\n", (unsigned long)sizeof(f));
-	printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(g));
-	printf("Size of a unsigned long: %lu byte(s)\n", (unsigned long)sizeof(h));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
