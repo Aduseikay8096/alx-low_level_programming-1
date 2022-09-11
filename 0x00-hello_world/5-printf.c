024e0fcfd003c53615584e22c9423353ec36cbcd
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-11 17:53:28 -0400

    List C data types

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,5 +7,17 @@
  */
 int main(void)
 {
-	
+	// Data types
+	char a;
+	double b;
+	float c;
+	int d;
+	short e;
+	unsigned short f;
+	long g;
+	unsigned long h;
+	long int i;
+	long long int j;
+
+
 }
