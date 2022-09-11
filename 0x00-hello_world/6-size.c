cec5edcb4ea88ff55962d761196eb1b325e51f54
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-11 18:39:48 -0400

    Make program Betty-compliant

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	// Data types
+/* Data types */
 	char a;
 	double b;
 	float c;
@@ -19,7 +19,7 @@ int main(void)
 	long int i;
 	long long int j;
 
-        /* Statements to check size of data type(s) */
+/* Statements to check size of data type(s) */
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
@@ -31,9 +31,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(j));
 	return (0);
-        /**
-         * Commands used to compile this program for different system architectures:
-         * 64-bit: gcc 6-size.c -m64 -o size64 2> /tmp/64
-         * 32-bit: gcc 6-size.c -m32 -o size32 2> /tmp/32
-         */
+/* Commands used to compile this program for different system architectures: */
+/* 64-bit: gcc 6-size.c -m64 -o size64 2> /tmp/64 */
+/* 32-bit: gcc 6-size.c -m32 -o size32 2> /tmp/32 */
 }
