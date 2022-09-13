0d1b7bf3b0c4b8d05ac4a98fad26777c4f15579a
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-13 19:22:02 -0400

    Debug programs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * main - print alphabets in lowercase
- * 
+ * print_alphabet_x10 - print alphabets in lowercase
+ *
  * Return: Always 0 (Success).
  */
 void print_alphabet_x10(void)
 {
-        int alpha;
+	int alpha, num;
 
-        for (alpha = 'a'; alpha <= 'z'; alpha++)
-        {
-                _putchar(alpha * 10);
-        }
-        _putchar('\n');
+	num = 0;
+
+	while (num < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		num++;
+		_putchar('\n');
+	}
 }
