0b7378ee94c72ca27b23318a1cf5324a7c3a06d6
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-13 19:26:17 -0400

    Update header file

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print alphabets in lowercase - 10 times
+ * 
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+        is_lower();
+        return (0);
+}
