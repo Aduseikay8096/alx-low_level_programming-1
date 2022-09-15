f6499ccfc7340d22816fe11448e83c10fb7d6184
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-14 22:55:52 -0400

    Pass Betty coding and documentation style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - print the last digit of a number.
- *@n: number being tested
+ *@m: number being tested
  * Return: Always 0 (Success).
  */
 
@@ -18,5 +18,5 @@ int print_last_digit(int m)
 	}
 
 	_putchar(n + '0');
-	return(n);
+	return (n);
 }
