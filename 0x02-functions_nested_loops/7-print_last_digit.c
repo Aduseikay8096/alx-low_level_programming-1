2f0bcceb0fec582c9dc8706f8a5eb6d2c8063064
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-14 22:52:33 -0400

    Fixe compilation error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success).
  */
 
-int print_last_digit(int n)
+int print_last_digit(int m)
 {
 	int n;
 
@@ -16,6 +16,7 @@ int print_last_digit(int n)
 	{
 		n = (-1 * n);
 	}
+
 	_putchar(n + '0');
-	return (n);
+	return(n);
 }
