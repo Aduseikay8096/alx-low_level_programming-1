c3bd717c4b3139773cf18bd2e8d7d21952a6ce22
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-09-14 22:09:19 -0400

    Update prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - function that prints alphabet in lowercase.
+ * print_alphabet - function that prints alphabet in lowercase 
  *
  * Return: Always 0 (Success).
  */
