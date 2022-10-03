62f45533213768c2361c53915d68b40856309a8b
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-03 17:06:07 -0400

    Create header file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Ikwuka
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libmy.a.
+ */
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif
