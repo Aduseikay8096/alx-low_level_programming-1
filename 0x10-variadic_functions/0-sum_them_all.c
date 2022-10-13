e695db1c0897d235ea1ad4d9a3bffe4fc76ae5e3
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-13 19:31:36 -0400

    Begin the debugging adventure

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,23 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum = 0;
-	va_list arg;
+	int add = 0, number;
+
+	va_list parameters;
+
+	va_start(parameters, n);
 
 	if (n == 0)
+	{
 		return (0);
-	va_start(arg, n);
+	}
+
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(arg, int);
+		number = va_arg(parameters, int);
+		add = add + number;
 	}
-	va_end(arg);
-	return (sum);
+
+	va_end(parameters);
+	return (add);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 int _putchar(int c);
 int sum_them_all(const unsigned int n, ...);
