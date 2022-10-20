d3ed11a91ac3476048679509f12f338bfe4ce820
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-20 17:11:40 -0400

    Fix bug

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void firstfunct(void) __attribute__((constructor));
 
 void firstfunct(void)
 {
-	printf("You're beat! and yet, you must allow\n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
