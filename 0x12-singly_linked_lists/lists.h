38ffc57a3faefc4f9cb349a723a6acc805bc645f
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-20 15:49:12 -0400

    Update signature file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,22 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ *
+ * @str: malloced string
+ * @len: length of string
+ * @next: points to next node
+ * Description: The singly linked node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t
+
 size_t print_list(const list_t *h);
 
 #endif /* _LISTS_H_ */
