f0dceb2db715c5865f69036d43238fe9bf9fa278
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-10-20 15:40:57 -0400

    Add vital header files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 size_t print_list(const list_t *h);
 
 #endif /* _LISTS_H_ */
