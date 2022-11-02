89c87275cc12a07bca06882d84aa166bfe15f63d
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-11-02 10:24:10 -0400

    Create main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#endif /* _MAIN_H_ */
