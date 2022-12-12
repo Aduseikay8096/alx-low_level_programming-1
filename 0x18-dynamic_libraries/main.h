2035ddeee22ec1e92c4239e0f352238f012d1893
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-12-12 12:37:32 -0500

    Add prototypes

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,4 +2,14 @@
 #define DYNAMIC_LIB
 #include <stdlib.h>
 
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+
+
 #endif /* DYNAMIC_LIB */
